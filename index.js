9757f58539d9da40fc9917756606cee43275139a
rhault <raullloredagomez@gmail.com>
2023-04-20 13:55:46 -0300

    add product to the home

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const productDetail = document.querySelector('.product-detail');
 const CLASS_HIDDEN = 'hidden';
+const addCardButton = './icons/btn_add_to_card.svg';
+const cardContainer = document.querySelector('.cards-container');
 
 //Functions
 
@@ -54,15 +56,48 @@ const createElement = (element, attributes = {}, ...nodes) => {
     return newElement
 }
 
-const products = [{name:computer, price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}];
+const products = [
+    {name:'Computer', price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:'Computer', price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:'Computer', price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:'Computer', price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:'Computer', price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:'Computer', price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name:'Computer', price:300, img:"https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+];
+
+/*
+<div class="product-card">
+    <img src="url" alt="">
+    <div  class="producto-info">
+        <div>
+            <p>$3000,00</p>
+            <p>Computador</p>
+        </div>
+        <figure>
+            <img src="./icons/btn_add_to_card.svg" alt="">
+        </figure>
+    </div>
+</div>
+*/
 
 for(const product of products){
     const {name, price, img} = product;
 
-    const productName = createElement('p', name);
-    const productPrice = createElement('p', price);
+    //<img src="url" alt=""></img>
     const productImg = createElement('img',{src:img});
-    
-        
-}
 
+    
+    const productPrice = createElement('p', {}, `$ ${price}`);
+    const productName = createElement('p', {}, name);
+    const divWrapper = createElement('div',{}, productPrice, productName);
+
+    const addToCard = createElement('img', {src: addCardButton})
+    const productFigure = createElement('figure', {}, addToCard);
+    
+    //<div  class="producto-info"></div>
+    const productInfo = createElement('div',{class:'producto-info'}, divWrapper, productFigure);
+
+    //<div class="product-card">
+    cardContainer.append(createElement('div', {class: 'product-card'}, productImg, productInfo));        
+}
